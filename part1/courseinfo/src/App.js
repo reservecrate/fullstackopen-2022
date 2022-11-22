c5b73c26c5b307f2fa7d5ccaf92e5e508869536f
Aldi Kabulov <reservecrate@gmail.com>
2022-11-22 07:55:03 +0600

    fixed props given to <Total/> component

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,24 +8,29 @@ const App = () => {
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   };
 
+  const total = course.parts.reduce((acc, part) => acc + part.exercises, 0);
+
   return (
-    <div> 
+    <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Total total={total} />
     </div>
   );
 };
